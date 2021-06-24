7bdcf7499ca3eed8b85196efe1fdf7e529220794
sunny nguyen-bui <snguyenbui@gmail.com>
2021-06-24 11:24:59 -0700

    tkinter grid

---
+++ minesweeper.py
@@ -0,0 +1,8 @@
+from tkinter import *
+import tkinter
+
+root = tkinter.Tk()
+for r in range(3):
+  for c in range(4):
+    tkinter.Label(root, text="R%s/C%s"%(r,c), borderwidth=1).grid(row=r,column=c)
+root.mainloop()
