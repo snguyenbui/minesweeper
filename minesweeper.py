d3018d0046917da875a27d3a16bb91cef1da856a
sunny nguyen-bui <snguyenbui@gmail.com>
2021-06-24 11:55:00 -0700

    start game button creates a grid

--- minesweeper.py
+++ minesweeper.py
@@ -1,8 +1,24 @@
 from tkinter import *
-import tkinter
+import tkinter as tk
 
-root = tkinter.Tk()
-for r in range(3):
-  for c in range(4):
-    tkinter.Label(root, text="R%s/C%s"%(r,c), borderwidth=1).grid(row=r,column=c)
-root.mainloop()
+class Game(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        canvas = tk.Canvas(self)
+        canvas.pack()
+        self.startGame = tk.Button(canvas, text="Start", background='white', font=("Helvetica"),
+                                   command=lambda: self.generate_grid(self.startGame, canvas))
+        self.startGame.place(x=150, y=0)
+
+    def hide_me(self, event):
+        print('hide me')
+        event.grid_forget()
+
+    def generate_grid(self, event, canvas):
+      for r in range(3):
+        for c in range(4):
+          cell = tk.Button(canvas, command=lambda: self.hide_me(cell)).grid(row=r,column=c)
+          canvas.pack()
+
+if __name__ == "__main__":
+    Game().mainloop()
