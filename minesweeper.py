10a919da537c90a40bb262a2bcd31fc9fbc165a7
sunny nguyen-bui <snguyenbui@gmail.com>
2021-06-24 12:15:02 -0700

    labels behind buttons

--- minesweeper.py
+++ minesweeper.py
@@ -6,29 +6,26 @@ class Game(tk.Tk):
         super().__init__()
         canvas = tk.Canvas(self)
         canvas.pack()
+        self.rows = 3
+        self.columns = 4
         self.startGame = tk.Button(canvas, text="Start", background='white', font=("Helvetica"),
                                    command=lambda: self.generate_grid(self.startGame, canvas))
         self.startGame.place(x=150, y=0)
 
-    def hide_me(self, event):
-        print('hide me')
-        event.grid_forget()
-
     def generate_grid(self, event, canvas):
-      for r in range(3):
-        for c in range(4):
+      for r in range(self.rows):
+        for c in range(self.columns):
+            tk.Label(canvas, text='R%s/C%s'%(r,c), borderwidth=1).grid(row=r,column=c,ipadx=8,ipady=14)
             Cell(canvas, r, c)
-            # cell = tk.Button(canvas, command=lambda: self.hide_me(cell)).grid(row=r,column=c)
             canvas.pack()
 
 class Cell():
   def __init__(self, canvas, r, c):
     super().__init__()
     self.cell = tk.Button(canvas, command=lambda: self.hide_me(self.cell))
-    self.cell.grid(row=r,column=c)
+    self.cell.grid(row=r,column=c,ipadx=20,ipady=10)
     
   def hide_me(self, event):
-    print('hide me')
     event.grid_forget()
 
 if __name__ == "__main__":
